9ced30920759e8d7e52b4633194f845a732353e8
ghostwalaa <ghostwalaa@gmail.com>
2025-11-06 01:31:53 +0530

    Create server.js

---
+++ server.js
@@ -0,0 +1,36 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    let filePath = '.' + req.url;
+    if (filePath === './') filePath = './index.html';
+
+    const contentTypes = {
+        '.html': 'text/html',
+        '.css': 'text/css',
+        '.js': 'text/javascript'
+    };
+
+    const extname = String(path.extname(filePath)).toLowerCase();
+    const contentType = contentTypes[extname] || 'application/octet-stream';
+
+    fs.readFile(filePath, (error, content) => {
+        if (error) {
+            if (error.code === 'ENOENT') {
+                res.writeHead(404);
+                res.end('File not found');
+            } else {
+                res.writeHead(500);
+                res.end('Server error: ' + error.code);
+            }
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(content, 'utf-8');
+        }
+    });
+});
+
+server.listen(8000, () => {
+    console.log('Server running at http://localhost:8000/');
+});
